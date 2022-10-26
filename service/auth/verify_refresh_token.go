8e7b4971ee53c573665c7a95ba182105e0d06ea8
tfkhdyt <tfkhdyt@pm.me>
2022-10-26 18:52:27 +0700

    fix(auth service): change error message on verify refresh token

--- service/auth/verify_refresh_token.go
+++ service/auth/verify_refresh_token.go
@@ -4,7 +4,7 @@ import "github.com/tfkhdyt/openmusic-go/exception"
 
 func (s Service) VerifyRefreshToken(token string) *exception.BadRequestError {
 	if err := s.repository.FindOne(token); err != nil {
-		return exception.NewBadRequestError("Token tidak valid")
+		return exception.NewBadRequestError("Refresh token tidak terdaftar di database")
 	}
 
 	return nil
