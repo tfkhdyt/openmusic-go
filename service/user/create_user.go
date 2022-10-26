df568000ce144c8330ef1c09cd3d9b373460eb9f
tfkhdyt <tfkhdyt@pm.me>
2022-10-26 17:05:53 +0700

    style(user service): remove unneeded curly braces

--- service/user/create_user.go
+++ service/user/create_user.go
@@ -9,19 +9,16 @@ import (
 
 func (s Service) Create(user *user.User) (string, *exception.InternalServerError) {
 	user.ID = "user-" + uuid.NewString()
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-	{
-		if err != nil {
-			return "", exception.NewInternalServerError("Gagal mengenkripsi password")
-		}
+	if err != nil {
+		return "", exception.NewInternalServerError("Gagal mengenkripsi password")
 	}
 	user.Password = string(hashedPassword)
 
 	userId, err := s.repository.Create(user)
-	{
-		if err != nil {
-			return userId, exception.NewInternalServerError("Gagal membuat user baru")
-		}
+	if err != nil {
+		return userId, exception.NewInternalServerError("Gagal membuat user baru")
 	}
 
 	return userId, nil
