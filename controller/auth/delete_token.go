06b5960520122b8a11039adec8f57e097415daf2
tfkhdyt <tfkhdyt@pm.me>
2022-10-27 09:31:12 +0700

    feat: add delete token feature

---
+++ controller/auth/delete_token.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tfkhdyt/openmusic-go/entity/auth"
+	"github.com/tfkhdyt/openmusic-go/util/response"
+)
+
+func (c Controller) DeleteToken(ctx *gin.Context) {
+	var auth auth.Auth
+
+	// validate request body
+	if err := ctx.ShouldBindJSON(&auth); err != nil {
+		response.SendFail(ctx, 400, err.Error())
+		return
+	}
+
+	// check token in database
+	if err := c.authService.VerifyRefreshToken(auth.Token); err != nil {
+		response.SendFail(ctx, err.StatusCode, err.Error())
+		return
+	}
+
+	// verify token
+	if _, err := c.tokenManager.VerifyJWT(auth.Token); err != nil {
+		response.SendFail(ctx, err.StatusCode, err.Error())
+		return
+	}
+
+	// delete token from database
+	if err := c.authService.DeleteToken(auth.Token); err != nil {
+		response.SendError(ctx, err)
+		return
+	}
+
+	// send success response
+	response.SendSuccessWithMessage(ctx, "Token berhasil dihapus")
+}
