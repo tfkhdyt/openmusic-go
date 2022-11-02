adec4ee6409568e0abbde5746cc5cab19185c188
tfkhdyt <tfkhdyt@pm.me>
2022-11-02 20:27:26 +0700

    fix(find all playlists): change owner to username on result

--- repository/postgres/playlist/find_all_playlists.go
+++ repository/postgres/playlist/find_all_playlists.go
@@ -2,13 +2,19 @@ package playlist
 
 import (
 	"github.com/tfkhdyt/openmusic-go/entity/playlist"
-	"github.com/tfkhdyt/openmusic-go/entity/user"
 )
 
-func (r Repository) FindAll(userId string) ([]playlist.Playlist, error) {
-	var user user.User
+func (r Repository) FindAll(userId string) ([]playlist.FindAllPlaylistsResult, error) {
+	/*
+		 	var user user.User
 
-	err := r.db.Preload("Playlists").First(&user, "id = ?", userId).Error
+			err := r.db.Preload("Playlists").First(&user, "id = ?", userId).Error
 
-	return user.Playlists, err
+			return user.Playlists, err
+	*/
+	result := make([]playlist.FindAllPlaylistsResult, 0)
+
+	err := r.db.Table("playlists p").Select("p.id", "p.name", "u.username").Joins("JOIN users u ON u.id = p.user_id").Where("p.user_id = ?", userId).Scan(&result).Error
+
+	return result, err
 }
