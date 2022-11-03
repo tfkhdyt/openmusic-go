54c1f97f751c2594d82fef9dd881d16f775f86b4
tfkhdyt <tfkhdyt@pm.me>
2022-11-03 15:02:58 +0700

    refactor(router): move routing from main.go to router package

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tfkhdyt/openmusic-go/db/postgres"
-	"github.com/tfkhdyt/openmusic-go/router/album"
-	"github.com/tfkhdyt/openmusic-go/router/auth"
-	"github.com/tfkhdyt/openmusic-go/router/playlist"
-	"github.com/tfkhdyt/openmusic-go/router/song"
-	"github.com/tfkhdyt/openmusic-go/router/user"
+	"github.com/tfkhdyt/openmusic-go/router"
 )
 
 func main() {
@@ -15,25 +11,7 @@ func main() {
 	db.Connect()
 	r := gin.Default()
 
-	albumRG := r.Group("/albums")
-	albumRouter := album.InitializeRouter()
-	albumRouter.Route(albumRG)
-
-	songRG := r.Group("/songs")
-	songRouter := song.InitializeRouter()
-	songRouter.Route(songRG)
-
-	userRG := r.Group("/users")
-	userRouter := user.InitializeRouter()
-	userRouter.Route(userRG)
-
-	authRG := r.Group("/authentications")
-	authRouter := auth.InitializeRouter()
-	authRouter.Route(authRG)
-
-	playlistRG := r.Group("/playlists")
-	playlistRouter := playlist.InitializeRouter()
-	playlistRouter.Route(playlistRG)
+	router.Route(r)
 
 	if err := r.Run(); err != nil {
 		panic(err.Error())
